c0c7d01e1301ec337b3e8ed2069a654b004f4f1d
microcai <microcai@fedoraproject.org>
2010-06-09 11:31:51 +0800

    convert editor_get_chinese_mode

--- src/editor.c
+++ src/editor.c
@@ -41,3 +41,39 @@ static void editor_init(Editor* editor)
 {
 
 }
+
+/*
+ * -1 非中文模式
+ * 0 简体中文
+ * 1 繁体中文
+ * 2 is Big charset mode, but simplify Chinese first
+ * 3 is Big charset mode, but traditional Chinese first
+ * 4 is Big charset mode.
+ */
+gint editor_get_chinese_mode()
+{
+  //到环境变量拿
+  char * lc = getenv("LC_CTYPE");
+  if(!lc)
+    lc = getenv("LANG");
+  lc = g_utf8_strdown(g_strdup(lc),-1);
+
+  char * zh = strstr(lc,"zh_");
+
+  if (zh)
+    {
+      //简体？繁体？
+      GStrv v = g_strsplit(zh,"_",3);
+
+      g_free(lc);
+
+      if(g_strcmp0(v[1],"cn")==0)
+      {
+        g_strfreev(v);
+        return 0;
+      }
+      g_strfreev(v);
+      return 1;
+    }
+  return -1;
+}
