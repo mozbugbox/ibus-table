2729dabb634f76fd900ee166e1c82101362d23d0
Caius 'kaio' Chance <cchance@redhat.com>
2010-06-12 07:00:05 +1000

    Added property.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 /* vim:set et sts=4: */
 
-
 #ifdef HAVE_CONFIG_H
 #include <config.h> // autotools
 #endif
@@ -34,21 +33,21 @@ GHashTable *name_2_db;
 IBusEngineDesc *
 ibus_table_make_engine(const gchar * tablename)
 {
-  gchar * name , * longname ,* description,*license,*author,*icon;
+  gchar * name, *longname, *description, *license, *author, *icon;
   // open db file
-  tabsqlitedb * db = tabsqlitedb_new(tablename,NULL);
+  tabsqlitedb * db = tabsqlitedb_new(tablename, NULL);
 
-  tabsqlitedb_getime(db,"name",&name);
-  tabsqlitedb_getime(db,"name.zh_cn",&longname);
-  tabsqlitedb_getime(db,"description",&description);
-  tabsqlitedb_getime(db,"author",&author);
-  tabsqlitedb_getime(db,"license",&license);
-  tabsqlitedb_getime(db,"icon",&icon);
+  tabsqlitedb_getime(db, "name", &name);
+  tabsqlitedb_getime(db, "name.zh_cn", &longname);
+  tabsqlitedb_getime(db, "description", &description);
+  tabsqlitedb_getime(db, "author", &author);
+  tabsqlitedb_getime(db, "license", &license);
+  tabsqlitedb_getime(db, "icon", &icon);
 
+  char * iconfile = g_strdup_printf("%s/%s", icondir, icon);
 
-  char * iconfile = g_strdup_printf("%s/%s",icondir,icon);
-
-  IBusEngineDesc * desc = ibus_engine_desc_new(name,longname,description,"zh",license,author,iconfile,"us");
+  IBusEngineDesc * desc = ibus_engine_desc_new(name, longname, description,
+      "zh", license, author, iconfile, "us");
 
   g_free(longname);
   g_free(description);
@@ -58,49 +57,57 @@ ibus_table_make_engine(const gchar * tablename)
 
   tabsqlitedb_destory(db);
 
-  g_hash_table_insert(name_2_db,name,g_strdup(tablename));
+  g_hash_table_insert(name_2_db, name, g_strdup(tablename));
   return desc;
 }
 
 static IBusComponent *
-ibus_table_get_component( const gchar * execfile , const gchar * table)
+ibus_table_get_component(const gchar * execfile, const gchar * table)
 {
-  glob_t globs =  { 0 };
+  glob_t globs =
+    { 0 };
 
   IBusComponent * component = ibus_component_new("org.freedesktop.IBus.Table",
-        _("Table Input Method"), PACKAGE_VERSION, "GPL", AUTHOR_EMAIL,
-        PACKAGE_BUGREPORT, execfile, GETTEXT_PACKAGE);
+      _("Table Input Method"), PACKAGE_VERSION, "GPL", AUTHOR_EMAIL,
+      PACKAGE_BUGREPORT, execfile, GETTEXT_PACKAGE);
 
   // change to table directory
   chdir(tabledbdir);
 
-  name_2_db = g_hash_table_new(g_str_hash,g_str_equal);
+  name_2_db = g_hash_table_new(g_str_hash, g_str_equal);
 
-  glob("*.db", GLOB_DOOFFS, NULL, &globs);
-
-  for (int i = 0; i < globs.gl_pathc; i++)
+  if (table)
     {
-      IBusEngineDesc * desc = ibus_table_make_engine(globs.gl_pathv[i]);
-      ibus_component_add_engine(component,desc);
+      IBusEngineDesc * desc = ibus_table_make_engine(table);
+      ibus_component_add_engine(component, desc);
     }
+  else
+    {
+      glob("*.db", GLOB_DOOFFS, NULL, &globs);
 
-  globfree(&globs);
-
+      for (int i = 0; i < globs.gl_pathc; i++)
+        {
+          IBusEngineDesc * desc = ibus_table_make_engine(globs.gl_pathv[i]);
+          ibus_component_add_engine(component, desc);
+        }
+      globfree(&globs);
+    }
   return component;
+
 }
 
 static void
-print_engines_xml (IBusComponent *component)
+print_engines_xml(IBusComponent *component)
 {
-    GString *output;
+  GString *output;
 
-    output = g_string_new ("");
+  output = g_string_new("");
 
-    ibus_component_output_engines (component, output, 0);
+  ibus_component_output_engines(component, output, 0);
 
-    write(STDOUT_FILENO,output->str,output->len);
+  write(STDOUT_FILENO, output->str, output->len);
 
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
 
 int
@@ -108,6 +115,7 @@ main(int argc, char* argv[])
 {
   // default if nth spec
 
+  IBusComponent *component;
   gboolean have_ibus = FALSE;
   gboolean have_xml = FALSE;
   gchar *locale_dir = NULL;
@@ -120,16 +128,16 @@ main(int argc, char* argv[])
     {
       { "ibus", '\0', 0, G_OPTION_ARG_NONE, &have_ibus },
       { "xml", '\0', 0, G_OPTION_ARG_NONE, &have_xml },
-      { "table", 't', 0, G_OPTION_ARG_STRING, &dbname,
-              _("name of the table."), N_("table name") },
-//========= for test
-      { "icon", '\0', 0, G_OPTION_ARG_STRING, &icondir,
-          _("Location of the logo."), N_("logofile") },
-      { "tabledir", '\0', 0, G_OPTION_ARG_STRING, &tabledbdir,
-          _("Location of the tables."), N_("table dir") },
-      { "locale", '\0', 0, G_OPTION_ARG_STRING, &locale_dir,
-          _("Path of the locale."), N_("locale") },
-      { 0 } };
+      { "table", 't', 0, G_OPTION_ARG_STRING, &dbname, _("name of the table."),
+          N_("table name") },
+    //========= for test
+          { "icon", '\0', 0, G_OPTION_ARG_STRING, &icondir,
+              _("Location of the logo."), N_("logofile") },
+          { "tabledir", '\0', 0, G_OPTION_ARG_STRING, &tabledbdir,
+              _("Location of the tables."), N_("table dir") },
+          { "locale", '\0', 0, G_OPTION_ARG_STRING, &locale_dir,
+              _("Path of the locale."), N_("locale") },
+          { 0 } };
 
   ibus_init();
 
@@ -142,8 +150,7 @@ main(int argc, char* argv[])
   if (locale_dir)
     bindtextdomain(GETTEXT_PACKAGE, locale_dir);
 
-
-  IBusComponent *component = ibus_table_get_component(argv[0],dbname);
+  component = ibus_table_get_component(argv[0],dbname);
 
   if (have_xml) // according to *.db generation for ibus usable xml desc file in dir
     {
@@ -156,11 +163,12 @@ main(int argc, char* argv[])
 
   IBusFactory *factory = ibus_factory_new(ibus_bus_get_connection(bus));
 
-  GList *p,*engines = ibus_component_get_engines (component);
-  for (p = engines; p != NULL; p = p->next) {
-      IBusEngineDesc *engine = (IBusEngineDesc *)p->data;
-      ibus_factory_add_engine (factory, engine->name, IBUS_TYPE_TABLE_ENGINE);
-  }
+  GList *p, *engines = ibus_component_get_engines(component);
+  for (p = engines; p != NULL; p = p->next)
+    {
+      IBusEngineDesc *engine = (IBusEngineDesc *) p->data;
+      ibus_factory_add_engine(factory, engine->name, IBUS_TYPE_TABLE_ENGINE);
+    }
 
   if (!have_ibus)
     {
